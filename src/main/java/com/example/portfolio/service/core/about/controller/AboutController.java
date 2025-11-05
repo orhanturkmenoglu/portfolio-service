9aaba5a21d4c81b23ce9a1c49e6de4c79fe2009b
orhan <orhantrkmn749@gmail.com>
2025-11-05 09:54:25 +0300

    Revert "feat(about): implement AboutController with createAbout and getAbouts endpoints using AboutService"
    This reverts commit f04dfde556365521e00f6f20fb1b0df31d396b87.

--- src/main/java/com/example/portfolio/service/core/about/controller/AboutController.java
+++ src/main/java/com/example/portfolio/service/core/about/controller/AboutController.java
@@ -4,17 +4,18 @@ import com.example.portfolio.service.core.about.dto.request.AboutRequestDTO;
 import com.example.portfolio.service.core.about.dto.response.AboutResponseDTO;
 import com.example.portfolio.service.core.about.service.AboutService;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/about")
+@RequestMapping("/abouts")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AboutController {
 
     private final AboutService aboutService;
@@ -23,5 +24,10 @@ public class AboutController {
     public ResponseEntity<AboutResponseDTO> createAbout(@Valid @RequestBody AboutRequestDTO aboutRequestDTO) {
         AboutResponseDTO aboutResponseDTO = aboutService.createAbout(aboutRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(aboutResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AboutResponseDTO>> getAbouts (){
+        return ResponseEntity.ok(aboutService.getAbouts());
     }
 }
--- src/main/java/com/example/portfolio/service/core/about/service/AboutService.java
+++ src/main/java/com/example/portfolio/service/core/about/service/AboutService.java
@@ -3,7 +3,11 @@ package com.example.portfolio.service.core.about.service;
 import com.example.portfolio.service.core.about.dto.request.AboutRequestDTO;
 import com.example.portfolio.service.core.about.dto.response.AboutResponseDTO;
 
+import java.util.List;
+
 public interface AboutService {
 
     AboutResponseDTO createAbout(AboutRequestDTO aboutRequestDTO);
+
+    List<AboutResponseDTO> getAbouts();
 }
